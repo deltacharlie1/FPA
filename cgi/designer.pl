e42207373961ada60ae37dff664a199be48d8487
Doug Conran <dconran@corunna.com>
2011-05-03 21:21:47 +0100

    more on pdf design and revamp frontpage

--- cgi/designer.pl
+++ cgi/designer.pl
@@ -9,33 +9,18 @@ $ACCESS_LEVEL = 0;
 use Checkid;
 $COOKIE =  &checkid($ENV{HTTP_COOKIE},$ACCESS_LEVEL);
 
-use DBI;
-my $dbh = DBI->connect("DBI:mysql:$COOKIE->{DB}");
-
-($Reg_id,$Com_id) = split(/\+/,$COOKIE->{ACCT});
-
-$Invoices = $dbh->prepare("select id as invid,invinvoiceno,invcusname,(invtotal+invvat - invpaid - invpaidvat) as invamount from invoices where invtype='S' and invstatuscode > 6 and acct_id=? order by invprintdate");
-$Invoices->execute("$COOKIE->{ACCT}");
-
 use Template;
 $tt = Template->new({
         INCLUDE_PATH => ['.','/usr/local/httpd/htdocs/fpa/lib']
 });
 
 $Vars = {
-        title => 'Accounts - Main Screen',
-	cookie => $COOKIE,
-        invoices => $Invoices->fetchall_arrayref({}),
-	coa => $Coa,
-	company => $Company,
-	reminders => $Reminder
+        title => 'Design Template'
 };
 
 
-print "Content-Type: text/html\n";
-print "Set-Cookie: fpa-comname=$Company->{comname}; path=/;\n\n";
+print "Content-Type: text/html\n\n";
 
 $tt->process('testit.tt',$Vars);
-$dbh->disconnect;
 exit;
 
--- cgi/designer2.pl
+++ cgi/designer2.pl
@@ -1,20 +1,42 @@
 #!/usr/bin/perl
 use PDF::API2;
 
-$pdf = PDF::API2->new;
-$page = $pdf->page();
-$page->mediabox('400,200');
-$g = $page->gfx();
-$g->strokecolor("#FF0000");
-$g->fillcolor("#ffffff");
+#$pdf = PDF::API2->new;
 
-$g->circle(70,70,20);
+$pdf = PDF::API2->open('testit.pdf');
+#$pdf = PDF::API2->new();
+
+$font = $pdf->corefont('Helvetica',1);
+$page = $pdf->openpage(1);
+$page->mediabox('A4');
 
 $text = $page->text();
-$text->translate(20,20);
-$font = $pdf->corefont('Helvetica',1);
-$text->font($font,24);
-$text->text("Hello World");
+$text->font($font,12);
+
+use CGI;
+$Data = new CGI;
+%FORM = $Data->Vars;
+
+while (( $Key,$Value) = each %FORM) {
+
+#  Remove any bad characters
+
+        $Value =~ s/\%2b/\+/ig;
+        $Value =~ tr/\\//d;
+        $Value =~ s/\'/\\\'/g;
+
+	if ($Value =~ /:\d+/) {
+		@Data = split(/\:/,$Value);
+		$text->translate($Data[1],832 - $Data[2]);
+		$text->text($Data[0]);
+warn "$Data[0] - x = $Data[1] - y = $Data[2]\n";
+
+	}
+}
+
+print "Content-Type: application/pdf\n";
+print "Content-Disposition: inline; filename=invoice_$Invoice_no.pdf\n\n";
+
 print $pdf->stringify();
 $pdf->end;
 
