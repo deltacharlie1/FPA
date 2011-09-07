ccca809211f3eccec1fc0a1970f13cc50f20a894
Doug Conran <dconran@corunna0.(none)>
2011-09-07 15:06:01 +0100

    Add Adverts page button to home page

--- htdocs/wp-content/themes/freeplusaccounts/inc_banner.php
+++ htdocs/wp-content/themes/freeplusaccounts/inc_banner.php
@@ -2,16 +2,18 @@
     <div class="banner" id="banner">
     
       <div class="m"><div class="banner-items"><div style="display:inline;float:left;"><img src="<?php bloginfo('template_directory'); ?>/images/banner-1.png" alt="Free Online Accountancy System" /></div>
-        <div id="media" style="position:relative;float:right; margin-top:54px;margin-right:60px;width:278px;height:200px;padding:0px;background-image:url(/fpaintro/frame1.png);background-repeat:none;">
+        <div id="media" style="position:relative;float:right; margin-top:26px;margin-right:60px;width:278px;height:200px;padding:0px;background-image:url(/fpaintro/frame1.png);background-repeat:none;">
          <div style="padding-left:11px;padding-top:12px;">
     <script type="text/javascript"> 
-AC_FL_RunContent( 'codebase','http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,28,0','width','256','height','144','src','/fpaintro/freeplus_player','quality','high','pluginspage','http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash','movie','/fpaintro/freeplus_player' ); //end AC code
+AC_FL_RunContent( 'codebase','http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,28,0','wmode','opaque','width','256','height','144','src','/fpaintro/freeplus_player','quality','high','pluginspage','http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash','movie','/fpaintro/freeplus_player' ); //end AC code
 </script><noscript><object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,28,0" width="256" height="144">
       <param name="movie" value="/fpaintro/freeplus_player.swf" />
       <param name="quality" value="high" />
       <embed src="fpaintro/freeplus_player.swf" quality="high" pluginspage="http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash" type="application/x-shockwave-flash" width="256" height="144"></embed>
     </object></noscript>
-        </div></div>
+        </div><div id="videobutton" title="Click to Play"></div>
+         <div style="padding-left:72px;padding-top:43px;"><a href="#" onclick="alert('Under Construction');"><img src="/images/advertspage.png"/></a></div>
+</div>
 </div></div>
       
       <div class="testimionials">
