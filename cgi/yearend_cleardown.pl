c6407e8c26fd877a41c5b0070e82a5fdb502fb5c
Doug Conran <doug.conran@corunna.com>
2012-10-19 23:30:35 +0100

    fix problem of fixed dates in yeaerend cleardown

--- cgi/yearend_cleardown.pl
+++ cgi/yearend_cleardown.pl
@@ -19,15 +19,10 @@ unless ($COOKIE->{NO_ADS}) {
 
 ($Reg_id,$Com_id) = split(/\+/,$COOKIE->{ACCT});
 
-$Dates = $dbh->prepare("select date_sub(comyearend,interval 1 year) as tbend,date_sub(date_add(comyearend, interval 1 day), interval 2 year) as tbstart,date_format(date_sub(comyearend,interval 1 year),'%d %M %Y') as dispend,date_format(date_sub(date_add(comyearend, interval 1 day), interval 2 year),'%d %M %Y') as dispstart,date_format(date_sub(comyearend,interval 1 year),'%d-%b-%y') as repend,date_format(date_sub(date_add(comyearend, interval 1 day), interval 2 year),'%d-%b-%y') as repstart from companies where reg_id=$Reg_id and id=$Com_id");
+$Dates = $dbh->prepare("select date_sub(comyearend,interval 1 year) as tbend,date_sub(date_add(comyearend, interval 1 day), interval 2 year) as tbstart,date_sub(date_add(comyearend, interval 1 day), interval 1 year) as fystart,date_format(date_sub(comyearend,interval 1 year),'%d-%b-%y') as dispend,date_format(date_sub(date_add(comyearend, interval 1 day), interval 2 year),'%d-%b-%y') as dispstart,date_format(date_sub(comyearend,interval 1 year),'%d-%b-%y') as repend,date_format(date_sub(date_add(comyearend, interval 1 day), interval 2 year),'%d-%b-%y') as repstart from companies where reg_id=$Reg_id and id=$Com_id");
 $Dates->execute;
 $Date = $Dates->fetchrow_hashref;
 $Dates->finish;
-$Date->{tbstart} = '2011-07-01';
-$Date->{tbend} = '2012-06-30';
-$Date->{fystart} = '2012-07-01';
-$Date->{dispstart} = '01-Jul-11';
-$Date->{dispend} = '30-Jun-12';
 
 $Coas = $dbh->prepare("select coas.coanominalcode as nominalcode,coadesc,coatype,sum(nominals.nomamount) as balance from coas left join nominals on (nominals.nomcode=coas.coanominalcode and nominals.acct_id=coas.acct_id) where nominals.nomdate>='$Date->{tbstart}' and nominals.nomdate<='$Date->{tbend}' and coanominalcode>'3999' and coas.acct_id='$COOKIE->{ACCT}' group by nominals.nomcode order by nominals.nomcode");
 $Coas->execute;
