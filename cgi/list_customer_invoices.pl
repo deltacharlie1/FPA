a80388f9597aec00873efe0622221b65974b28d2
Doug Conran <doug.conran@corunna.com>
2014-04-29 16:16:59 +0100

    Allow multi-page invoices for individual customers {list_customer_invoices)

--- cgi/list_customer_invoices.pl
+++ cgi/list_customer_invoices.pl
@@ -7,17 +7,39 @@ $ACCESS_LEVEL = 1;
 use Checkid;
 $COOKIE = &checkid($ENV{HTTP_COOKIE},$ACCESS_LEVEL);
 
+use Checkid;
+$COOKIE = &checkid($ENV{HTTP_COOKIE},$ACCESS_LEVEL);
+
+$Buffer = $ENV{QUERY_STRING};
+
+@pairs = split(/&/,$Buffer);
+
+foreach $pair (@pairs) {
+
+        ($Name, $Value) = split(/=/, $pair);
+
+        $Value =~ tr/+/ /;
+        $Value =~ s/%([a-fA-F0-9][a-fA-F0-9])/pack("C",hex($1))/eg;
+        $Value =~ tr/\\\'//d;
+        $FORM{$Name} = $Value;
+}
+
 use DBI;
 $dbh = DBI->connect("DBI:mysql:$COOKIE->{DB}");
 
 $Customers = $dbh->prepare("select id,cusname,cusaddress,cuspostcode,cusregion,custel,cuscontact,cusemail,custerms,cusbalance,cuscredit,cuslimit,cusdefpaymethod,cuscis from customers where id=? and acct_id=?");
-$Customers->execute($ENV{QUERY_STRING},"$COOKIE->{ACCT}");
+$Customers->execute($FORM{cus},"$COOKIE->{ACCT}");
 
-$Invoices = $dbh->prepare("select invoices.id as invid,invinvoiceno,invtype,invdesc,date_format(invprintdate,'%d-%b-%y') as printdate,date_format(invduedate,'%d-%b-%y') as duedate,(invtotal+invvat) as invamount,invstatus,(invpaid+invpaidvat) as invpaid,to_days(invprintdate) as printdays,to_days(invduedate) as duedays,invstatuscode,date_format(max(itdate),'%d-%b-%y') as itdate,((invtotal*0.8)+invvat) as cisamount from invoices left join inv_txns on (inv_txns.inv_id=invoices.id and inv_txns.acct_id=invoices.acct_id) where invinvoiceno <> 'unlisted' and invtype in ('S','C') and cus_id=? and invoices.acct_id=? group by invoices.id order by invstatuscode desc,invprintdate desc limit 0,24");
-$Invoices->execute($ENV{QUERY_STRING},"$COOKIE->{ACCT}");
-$Numrows = $Invoices->rows;
-$Rows = 24;
-$Offset = 0;
+unless ($FORM{rows}) {
+	$Invoices = $dbh->prepare("select * from invoices where  invinvoiceno <> 'unlisted' and invtype in ('S','C') and cus_id=$FORM{cus} and acct_id='$COOKIE->{ACCT}'");
+	$Invoices->execute;
+	$FORM{numrows} = $Invoices->rows;
+	$FORM{offset} = 0;
+	$FORM{rows} = 24;
+}
+
+$Invoices = $dbh->prepare("select invoices.id as invid,invinvoiceno,invtype,invdesc,date_format(invprintdate,'%d-%b-%y') as printdate,date_format(invduedate,'%d-%b-%y') as duedate,(invtotal+invvat) as invamount,invstatus,(invpaid+invpaidvat) as invpaid,to_days(invprintdate) as printdays,to_days(invduedate) as duedays,invstatuscode,date_format(max(itdate),'%d-%b-%y') as itdate,((invtotal*0.8)+invvat) as cisamount from invoices left join inv_txns on (inv_txns.inv_id=invoices.id and inv_txns.acct_id=invoices.acct_id) where invinvoiceno <> 'unlisted' and invtype in ('S','C') and cus_id=? and invoices.acct_id=? group by invoices.id order by invstatuscode desc,invprintdate desc limit $FORM{offset},$FORM{rows}");
+$Invoices->execute($FORM{cus},"$COOKIE->{ACCT}");
 
 use Template;
 $tt = Template->new({
@@ -31,9 +53,9 @@ $Vars = {
 	cookie => $COOKIE,
 	cus => $Customers->fetchrow_hashref,
 	invoices => $Invoices->fetchall_arrayref({}),
-	numrows => $Numrows,
-	offset => $Offset,
-	rows => $Rows,
+	numrows => $FORM{numrows},
+	offset => $FORM{offset},
+	rows => $FORM{rows},
         javascript => '<script language="JavaScript">
 var responseText = "";
 var errfocus = "";
@@ -81,9 +103,9 @@ $(document).ready(function(){
 });
 function redisplay(action) {
 
-  numrows = ' . $Numrows . ';
-  offset = ' . $Offset . ';
-  rows = ' . $Rows . ';
+  numrows = ' . $FORM{numrows} . ';
+  offset = ' . $FORM{offset} . ';
+  rows = ' . $FORM{rows} . ';
   srch = "' . $Search . '";
 
   if (document.getElementById("goto").value.length > 0) {
@@ -133,7 +155,7 @@ function redisplay(action) {
       break;
   }
 
-  location.href = "/cgi-bin/fpa/list_customer_invoices.pl?numrows=" + numrows + "&offset=" + offset + "&rows=" + rows + "&search=" + srch;
+  location.href = "/cgi-bin/fpa/list_customer_invoices.pl?cus='.$FORM{cus}.'&numrows=" + numrows + "&offset=" + offset + "&rows=" + rows + "&search=" + srch;
 }
 function get_amt(amtinvid,amtinvno,amtamt) {
   document.getElementById("i_id").value = amtinvid;
