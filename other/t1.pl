60a6c6577a575f1e45ec4e8ff4d850764ceff9bd
Doug Conran <doug.conran49@googlemail.com>
2019-09-03 15:21:42 +0100

    Tidying up

--- other/t1.pl
+++ other/t1.pl
@@ -1,10 +1,6 @@
 #!/usr/bin/perl
 
-use JSON;
+$dte = "21/01/19";
+$dte =~ s/(\d\d)\/(\d\d)\/(\d\d)/20$3-$2-$1/;
 
-$Json_text = '{"redirect_flows":{"id":"RE0000CTS1AT5DJY29WNT57BBWWHQVA2","description":"FreePlus Standard","session_token":"SESS_wSs0uGYMISxzqOBq","scheme":null,"success_redirect_url":"https://example.com/pay/confirm","created_at":"2017-03-26T18:03:26.226Z","links":{"creditor":"CR00004M4FD1KK"},"redirect_url":"https://pay-sandbox.gocardless.com/flow/RE0000CTS1AT5DJY29WNT57BBWWHQVA2"}}';
-
-$ptext = from_json($Json_text);
-
-print $ptext->{"redirect_flows"}->{"description"}."\n";
-
+print $dte."\n";
